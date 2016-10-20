8356096780d7dedb642cfa0031d3c7479cec97d7
Therese Diede <tdiede@gmail.com>
2016-10-20 22:31:40 +0000

    Setup initialized.

---
+++ model.py
@@ -0,0 +1,99 @@
+"""Models and database functions for BENTO BOX project."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+# This is the connection to the PostgreSQL database; we're getting this through
+# the Flask-SQLAlchemy helper library. On this, we can find the `session`
+# object, where we do most of our interactions (like committing).
+
+db = SQLAlchemy()
+
+
+##############################################################################
+# Model definitions
+
+class User(db.Model):
+    """User of website."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+
+    email = db.Column(db.String(64), nullable=True, unique=True)
+    password = db.Column(db.String(64), nullable=True)
+    age = db.Column(db.Integer, nullable=True)
+    architecture_degree = db.Column(db.Boolean, nullable=True)
+
+    def __repr__(self):
+        """Provides helpful representation when printed."""
+
+        return "<User user_id=%s email=%s>" % (self.user_id, self.email)
+
+
+class Flashcard(db.Model):
+    """Flashcards made from Content, stored under User profile."""
+
+    __tablename__ = "flashcards"
+
+    flashcard_id = db.Column(db.Integer,
+                             autoincrement=True,
+                             primary_key=True)
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('users.user_id'))
+    content_id = db.Column(db.Integer,
+                           db.ForeignKey('contents.content_id'))
+
+    # Define relationship to user.
+    user = db.relationship("User",
+                           backref=db.backref("flashcards",
+                                              order_by=flashcard_id))
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Flashcard flashcard_id=%s name=%s>" % (self.flashcard_id, self.name)
+
+
+class Content(db.Model):
+    """Content of flashcards."""
+
+    __tablename__ = "contents"
+
+    content_id = db.Column(db.Integer,
+                           autoincrement=True,
+                           primary_key=True)
+    title = db.Column(db.String(128))
+    question = db.Column(db.String(256))
+    answer = db.Column(db.String(256))
+    img = db.Column(db.String(256))
+    category = db.Column(db.String(128))
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        s = "<Content content_id=%s title=%s question=%s answer=%s>"
+        return s % (self.content_id, self.title, self.question, self.answer)
+
+
+##############################################################################
+# Helper functions
+
+def connect_to_db(app, db_uri="postgresql:///flashcards"):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PstgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app
+    connect_to_db(app)
+    print "Connected to DB."
---
+++ server.py
@@ -0,0 +1,165 @@
+"""ADU bento box."""
+
+from jinja2 import StrictUndefined
+
+from flask import (Flask, render_template, request, flash, redirect, session)
+from flask_debugtoolbar import DebugToolbarExtension
+
+from model import connect_to_db, db
+from model import (User, Flashcard, Content)
+
+
+app = Flask(__name__)
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+# Normally, if you use an undefined variable in Jinja2, it fails silently.
+# This is horrible. Fix this so that, instead, it raises an error.
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def index():
+    """Homepage."""
+
+    return render_template("homepage.html")
+
+
+@app.route('/register', methods=['GET'])
+def register_form():
+    """New users can create an account through this form."""
+
+    return render_template("register_form.html")
+
+
+@app.route('/register', methods=['POST'])
+def register_process():
+    """Process new user account registration."""
+
+    # Get form variables.
+    email = request.form['email']
+    password = request.form['password']
+    age = int(request.form['age'])
+    zipcode = request.form['zipcode']
+
+    new_user = User(email=email, password=password, age=age, zipcode=zipcode)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    flash("User %s added." % email)
+    return redirect("/")
+
+
+@app.route('/login', methods=['GET'])
+def login_form():
+    """Registered users can log in through this form."""
+
+    return render_template("login_form.html")
+
+
+@app.route('/login', methods=['POST'])
+def login_process():
+    """Process user login."""
+
+    # Get form variables.
+    email = request.form['email']
+    password = request.form['password']
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        flash("You are not a registered user, %s. Please register." % email)
+        return redirect("/register")
+
+    if user.password != password:
+        flash("Incorrect password. Please try again.")
+        return redirect("/login")
+
+    session['user_id'] = user.user_id
+
+    flash("Logged in.")
+    return redirect("/users/%s" % user.user_id)
+
+
+@app.route('/logout', methods=['POST'])
+def logout_process():
+    """Process user logout."""
+
+    del session['user_login']
+    flash('You are now logged out.')
+    return redirect("/")
+
+
+@app.route('/users')
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+
+    return render_template("user_list.html", users=users)
+
+
+@app.route('/users/<int:user_id>')
+def user_profile(user_id):
+    """Show user profile and information."""
+
+    user = User.query.get(user_id)
+    return render_template("user.html", user=user)
+
+
+@app.route('/flashcards')
+def show_flashcards():
+    """Show list of flashcards."""
+
+    flashcards = Flashcard.query.order_by('name').all()
+
+    return render_template("flashcards.html", flashcards=flashcards)
+
+
+@app.route('/flashcards/<int:flashcard_id>', methods=['GET'])
+def test_flashcard(flashcard_id):
+
+    flashcard = Flashcard.query.get(flashcard_id)
+
+    return render_template("flashcard.html",
+                           flashcard=flashcard)
+
+
+@app.route('/flashcards/<int:flashcard_id>', methods=['POST'])
+def make_flashcard(flashcard_id):
+    """Add (or edit) a flashcard."""
+
+    # Get form variables.
+    content_id = int(request.form['content_id'])
+    knowledge_score = int(request.form['knowledge_score'])
+
+    user_id = session.get('user_id')
+    if not user_id:
+        raise Exception("No user logged in.")
+
+    content = Content.query.filter_by(content_id).first()
+    flashcard = Flashcard(user_id=user_id, content=content, knowledge_score=knowledge_score)
+    flash('Flashcard added to your deck.')
+    db.session.add(flashcard)
+    db.session.commit()
+
+    return redirect("/flashcards/%s" % flashcard_id)
+
+
+################################################################################
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(host='0.0.0.0')
